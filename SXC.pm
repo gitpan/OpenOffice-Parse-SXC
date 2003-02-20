cc6d31460d5f508d22f38ccfcf373ca40af7cbc5
Desmond Lee <neener666@hotmail.com>
2003-02-20 01:06:20 +0000

    Import of DCLEE/OpenOffice-Parse-SXC-0.03 from CPAN.
    gitpan-cpan-distribution: OpenOffice-Parse-SXC
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         DCLEE/OpenOffice-Parse-SXC-0.03.tar.gz
    gitpan-cpan-author:       DCLEE
    gitpan-cpan-maturity:     released

--- SXC.pm
+++ SXC.pm
@@ -24,7 +24,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw(
 );
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 my %valid_options	= ( worksheets	=> 1,
 			    no_trim	=> 1,
